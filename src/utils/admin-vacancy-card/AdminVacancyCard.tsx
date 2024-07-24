3dacc8cb82b8c5b532c31aa29839c1acf8c6e3e2
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-07-24 17:51:05 +0500

    Bug fixed

--- src/utils/admin-vacancy-card/AdminVacancyCard.tsx
+++ src/utils/admin-vacancy-card/AdminVacancyCard.tsx
@@ -3,9 +3,13 @@ import "./AdminVacancyCard.scss"
 import { IoMdTime } from "react-icons/io"
 import { GiMoneyStack } from "react-icons/gi"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 const AdminVacancyCard = ({adminVacancy}: any) => {
-  return (
-    <div className="admin__vacancy-card">
+
+  const [isDeleted, setIsDeleted] = useState<boolean>(false)
+
+  return isDeleted ? null : (
+    <div  className="admin__vacancy-card">
         <h3 className="job-title">{adminVacancy.job_title}</h3>
         <div className="date-action">
             <div className="day-item">
@@ -27,7 +31,7 @@ const AdminVacancyCard = ({adminVacancy}: any) => {
         </div>
         <div className="card-action">
           <Link to={`result/${adminVacancy.id}`} className="more-btn">Batafsil</Link>
-          <button className="delete-btn">O'chirish</button>
+          <button onClick={() => setIsDeleted(true)}  className="delete-btn">O'chirish</button>
         </div>
     </div>
   )
